68232506c22e72672783a3893592e7aeb83765d8
supriya469 <supriyasalunkhe0511@gmail.com>
2024-10-24 10:40:08 +0530

    Add files via upload
    This is the navigation menu task,it has fixed navigation bar

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change navbar background color when scrolling
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#111';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+});
